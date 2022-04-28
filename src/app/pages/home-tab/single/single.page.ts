2fa5881d5af87069a3e1b5cd22a225f6ce6bba12
sofo <soffoalbert@gmail.com>
2022-04-28 19:00:06 +0200

    wonking on read more for long text

--- src/app/pages/home-tab/single/single.page.ts
+++ src/app/pages/home-tab/single/single.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NavController} from '@ionic/angular';
 import {Booking} from '../../entities/booking';
 import {SearchServicesModel} from '../../search-services-tab/search-services.model';
@@ -13,6 +13,13 @@ import {BookingProvider} from '../booking/booking.provider';
 })
 export class SinglePage implements OnInit {
   searchServicesModels: SearchServicesModel[];
+  lastIndexName = 20;
+  lastIndexLocation = 100;
+  counterLocation = 100;
+  counterName = 20;
+  firstCount = 20;
+  location;
+  showTxt = 'Show More';
 
   constructor(public navController: NavController, private activatedRoute: ActivatedRoute, private bookingProvider: BookingProvider
   ) {
@@ -21,7 +28,42 @@ export class SinglePage implements OnInit {
   ngOnInit(): void {
     this.activatedRoute.data.subscribe((response) => {
       this.searchServicesModels = response.data;
+      this.searchServicesModels.forEach((service) => {
+        this.lastIndexName = (service.name.substring(0, 20)).lastIndexOf('');
+        this.lastIndexLocation = (service.location.substring(0, 20)).lastIndexOf('');
+        if (this.lastIndexName > 20) {
+          this.lastIndexName = 20;
+        }
+        if (this.lastIndexLocation > 20) {
+          this.lastIndexLocation = 20;
+        }
+        this.counterLocation = this.lastIndexLocation;
+        this.counterName = this.lastIndexName;
+      });
     });
+  }
+
+  toggleNameLength($event: MouseEvent, service: SearchServicesModel) {
+    if (this.counterName < 21) {
+
+        this.counterName = service.name.length;
+        this.showTxt = 'Show less';
+
+      } else {
+        this.counterName = this.lastIndexName;
+
+        this.showTxt = 'Show More';
+      }
+  }
+
+  toggleLocationLength($event: MouseEvent, service: SearchServicesModel) {
+    if (this.counterLocation < 21) {
+        this.counterLocation = service.location.length;
+        this.showTxt = 'Show less';
+      } else {
+        this.counterLocation = this.lastIndexLocation;
+        this.showTxt = 'Show More';
+      }
   }
 
   openBooking(serviceId: number) {
